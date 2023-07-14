d9ffe7e8c68cd5838b6b5830caaa27eed16aac51
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-14 11:04:44 +0200

    If- else- Conditions

--- script_01.js
+++ script_01.js
@@ -5,7 +5,7 @@
 // Deklaration + Assignment
 
 const ageJohn = 20;
-const ageMark = 30;
+const ageMark = 18;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
@@ -20,3 +20,28 @@ console.log("ageMark: " + ageMark);
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
 console.log("------------------");
+
+/************ IF ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(isJohnOlder)
+// {
+//     console.log("John ist älter!"); 
+// }
+
+/************ IF - ELSE ************/
+// mit Alternative
+// entweder ja oder nein
+
+// if (true)
+// if (false)  
+if (isJohnOlder) 
+{
+    console.log("John ist älter!");
+}
+else 
+{
+    console.log("John ist jünger!");
+}
