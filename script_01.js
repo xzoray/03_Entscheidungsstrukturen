c1d6a258ca17756cf59764f77bfc5792359888ac
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-14 09:18:27 +0200

    Wer ist älter?

--- script_01.js
+++ script_01.js
@@ -3,3 +3,20 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+
+const ageJohn = 20;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
