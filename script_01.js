f2b57153d4655beb1d8456dd20693fe78d24e0d5
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-14 12:20:22 +0200

    switch- case

--- script_01.js
+++ script_01.js
@@ -4,22 +4,22 @@
 
 // Deklaration + Assignment
 
-const ageJohn = 20;
-const ageMark = 20;
+// const ageJohn = 20;
+// const ageMark = 20;
 
-// Deklaration
-let isJohnOlder, isJohnEqual;
+// // Deklaration
+// let isJohnOlder, isJohnEqual;
 
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
+// // Ausgabe
 
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF ************/
 // TINA --> There is no alternative!
@@ -37,18 +37,51 @@ console.log("------------------");
 
 // if (true)
 // if (false)  
-if (isJohnOlder) 
-{
-    console.log("John ist älter!");
-}
-else if (isJohnEqual) 
-{
-    console.log("beide gleich alt!");
-}
-else 
-{
-    console.log("John ist jünger!");
-}
+// if (isJohnOlder) 
+// {
+//     console.log("John ist älter!");
+// }
+// else if (isJohnEqual) 
+// {
+//     console.log("beide gleich alt!");
+// }
+// else 
+// {
+//     console.log("John ist jünger!");
+// }
 
 /************ Ternäre (ternary) Schreibweise ************/ 
 // console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = prompt("Was macht Jane?");
+
+// job = "driver";         // .. fährt TAXI! / UBER
+// job = "diver";          // .. taucht im Rhein!
+// job = "artist";         // .. malt ein Bild!
+// job = "pilot";          // .. macht etwas anderes! --> default
+// job = "teacher";        // .. unterrichtet!
+// job = "instructor";     // .. unterrichtet!
+
+switch (job) {
+    case "driver":
+        console.log("Jane fährt TAXI! / UBER!");
+        break;
+    case "diver":
+        console.log("Janetaucht im Rhein!");
+        break;
+        case "artist":
+            console.log("Jane malt ein Bild!!");
+            break;
+        case "stripper":
+            console.log("Jane ist Anders!");
+            break;
+    default:
+        console.log("Ab zum Arbeitsamt!");
+        break;
+}
